aff7f7a6b33b55a3b6f7ef2427b82a22c9c3fffb
joshua <zaitochegne@gmail.com>
2024-11-30 11:28:33 -0500

    se actualiza todos los estilos de la seccion proyectos

--- src/components/CardProyectsContainer.jsx
+++ src/components/CardProyectsContainer.jsx
@@ -1,9 +1,9 @@
 import style from '../style/cardProyectContainer.module.css'
 import { CardProyect } from './CardProyect'
 const listImages = [
-    {url:'/img/card/garfield6.jpeg',text:'youbyte'},
-    {url:'/img/card/garfield2.jpeg',text:'wolfbyte'},
-    {url:'/img/card/garfield3.jpeg',text:'landing'}
+    {url:'/img/screen-capture.gif',text:'youVideo'},
+    {url:'/img/screen-capture.gif',text:'wolfbyte'},
+    {url:'/img/screen-capture2.gif',text:'wolfbyte'}
 ]
 
 export default function CardProyectsContainer() {
